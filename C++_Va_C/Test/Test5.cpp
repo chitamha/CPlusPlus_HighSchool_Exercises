61c34d1b143ff74aa69bfb8f3996afaa12b11d8c
chitamha <hachitam.me@gmail.com>
2024-11-10 19:18:09 +0700

    10_11_2024

--- C++_Va_C/Test/Test5.cpp
+++ C++_Va_C/Test/Test5.cpp
@@ -9,6 +9,17 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-
+    int F[5][1000], ans=0;
+    memset(F, 0, sizeof F);
+    for (int i=1; i<=4; i++) F[i][1]=1;
+    for (int i=2; i<=7; i++){
+        for (int j=1; j<=4; j++){
+            for (int k=1; k<=4; k++){
+                if (j!=k) F[j][i]+=F[k][i-1];
+            }
+        }
+    }
+    for (int i=1; i<=4; i++) ans+=F[i][7]*3;
+    cout<< ans;
     return 0;
 }
