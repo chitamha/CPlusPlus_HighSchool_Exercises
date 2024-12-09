c869b08f4c5aaae6d8634421123d804abee51e57
chitamha <hachitam.me@gmail.com>
2024-12-09 23:18:05 +0700

    09_12_2024

--- C++_Va_C/Test/Test4.cpp
+++ C++_Va_C/Test/Test4.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
-#define llll pair<ll, ll>
-#define ll long long
-#define maxn 1000006
+#define maxn 50000006
 using namespace std;
+
+int n;
+vector<int> A, v;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-
+    freopen("TOWER3.INP", "r", stdin);
+    freopen("TOWER2.OUT", "w", stdout);
+    cin>> n;
+    for (int i=1; i<=n; i++){
+        int r, h; cin>> r>> h;
+        A.insert(A.end(), h, r);
+    }
+    for (int i=A.size()-1; i>=0; i--){
+        auto it=upper_bound(v.begin(), v.end(), A[i]);
+        if (it==v.end()) v.push_back(A[i]);
+        else *it=A[i];
+    }
+    cout<< v.size();
     return 0;
 }
--- C++_Va_C/Test/Test5.cpp
+++ C++_Va_C/Test/Test5.cpp
@@ -1,14 +1,56 @@
 #include <bits/stdc++.h>
+#define maxn 100005
 #define fi first
 #define se second
-#define llll pair<ll, ll>
-#define ll long long
-#define maxn 1000006
 using namespace std;
 
+int n, ans=0;
+vector<pair<int, int>> A;
+multiset<pair<int, int>> v;
+
 int main(){
+    //freopen("TOWER3.INP", "r", stdin);
+    //freopen("TOWER1.OUT", "w", stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-
+    cin>> n;
+    for (int i=1; i<=n; i++){
+        int r, h; cin>> r>> h;
+        A.push_back({r, h});
+    }
+    for (int i=A.size()-1; i>=0; i--){
+        auto it=v.upper_bound(A[i]);
+        if (it==v.end()) v.insert(A[i]);
+        else{
+            if ((*it).se==A[i].se){
+                v.erase(it);
+                v.insert(A[i]);
+            } if ((*it).se>A[i].se){
+                pair<int, int> tmp={(*it).fi, (*it).se-A[i].se};
+                v.erase(it);
+                v.insert(tmp);
+                v.insert(A[i]);
+            } else{
+                int sum=0, cnt=0;
+                auto en=it;
+                while (sum<=A[i].se && en!=v.end()){
+                    sum+=(*en).se;
+                    en++; cnt++;
+                }
+                int check=1?0:en==v.end();
+                for (int j=1; j<=cnt-1; j++) v.erase(it);
+                if (check==0){
+                    pair<int, int> tmp={(*it).fi, sum-A[i].se};
+                    v.erase(it);
+                    if (tmp.se>0) v.insert(tmp);
+                }
+                v.insert(A[i]);
+            }
+        }
+    }
+    //cout<< "Here"<< endl;
+    //for (auto x:v) cout<< x.fi<< " "<< x.se<< endl;
+    for (auto x:v) ans+=x.se;
+    cout<< ans;
     return 0;
 }
