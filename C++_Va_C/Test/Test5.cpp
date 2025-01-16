553dfe58394d04d1dafcd22320656babaef3cd88
chitamha <hachitam.me@gmail.com>
2025-01-16 23:09:44 +0700

    16_01_2025

--- C++_Va_C/Test/Test5.cpp
+++ C++_Va_C/Test/Test5.cpp
@@ -1,38 +1,14 @@
 #include <bits/stdc++.h>
-#define maxn 100005
 #define fi first
 #define se second
+#define llll pair<ll, ll>
+#define ll long long
+#define maxn 1000006
 using namespace std;
-
-int n, ans=0;
-vector<int> A;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-    cin>> n;
-    A.resize(n+5);
-    for (int i=1; i<=n; i++) cin>> A[i];
-    int l=1, r=n, k;
-    while (l<=r){
-        k=(l+r)/2;
-        deque<int> dq;
-        int check=0;
-        for (int i=1; i<=n; i++){
-            while (!dq.empty() && A[i]<=A[dq.back()]) dq.pop_back();
-            dq.push_back(i);
-            if (dq.front()<=i-k) dq.pop_front();
-            if (i>=k && A[dq.front()]>=k){
-                ans=max(ans, k);
-                l=k+1;
-                check=1;
-                break;
-            }
-        }
-        if (check==0) r=k-1;
-        dq.clear();
-        //cout<< k<< " "<< ans<< endl;
-    }
-    cout<< ans;
+
     return 0;
 }
