81df0c9f0d051508f6255103033eaa1d66e17bc0
chitamha <hachitam.me@gmail.com>
2025-02-13 15:45:45 +0700

    13_02_2025

--- C++_Va_C/ThayQuang/SetMap/5_Bai5_DemCapPhanTu/cpairs.cpp
+++ C++_Va_C/ThayQuang/SetMap/5_Bai5_DemCapPhanTu/cpairs.cpp
@@ -5,18 +5,10 @@
 using namespace std;
 
 ll n, p, k, ans=0;
-vector<pair<ll, ll>> AA2;
+map<ll, ll> mp;
 
-ll Find(ll id, ll sum){
-    ll l=0, r=id-1, mid=0;
-    while (l<=r){
-        mid=(l+r)/2;
-        ll tmp=(AA2[mid].fi+AA2[id].fi)*(AA2[mid].se+AA2[id].se);
-        if (tmp==sum) return 1;
-        if (tmp>sum) r=mid-1;
-        else l=mid+1;
-    }
-    return 0;
+ll Mod(ll x){
+    return ((((x*x)%p)*((x*x)%p)%p)-(k*x)%p+p)%p;
 }
 
 int main(){
@@ -27,13 +19,8 @@ int main(){
     cin>> n>> p>> k;
     for (ll i=1; i<=n; i++){
         ll x; cin>> x;
-        AA2.push_back({x%p, (x*x)%p});
-    }
-    sort(AA2.begin(), AA2.end());
-    for (ll i=1; i<n; i++){
-        for (ll j=0; j<=4; j++){
-            ans+=Find(i, j*p+k);
-        }
+        mp[Mod(x)]++;
+        ans+=mp[Mod(x)]-1;
     }
     cout<< ans;
     return 0;
