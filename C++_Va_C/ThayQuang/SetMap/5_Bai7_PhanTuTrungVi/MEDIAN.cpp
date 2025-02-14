a166a4b7cfb5e767bcc8a92f5a66c1f94eb07173
chitamha <hachitam.me@gmail.com>
2025-02-14 23:25:24 +0700

    14_02_2025

---
+++ C++_Va_C/ThayQuang/SetMap/5_Bai7_PhanTuTrungVi/MEDIAN.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fi first
+#define se second
+using namespace std;
+
+int main(){
+    freopen("median.inp", "r", stdin);
+    freopen("median.out", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    return 0;
+}
