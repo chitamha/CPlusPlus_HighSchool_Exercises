fb4c5b26df6f364ef67685a2ce74487efe16b006
chitamha <hachitam.me@gmail.com>
2025-03-01 06:25:34 +0700

    01_03_2025

--- C++_Va_C/Test/Test3.cpp
+++ C++_Va_C/Test/Test3.cpp
@@ -6,19 +6,24 @@
 #define maxn 1000005
 using namespace std;
 
-int n, t, F[1000005];
+ll n, k, F[1005][1005], MOD=1000000007;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
-    for (int i=1; i<=1000000; i++){
-        F[i]=F[i-1];
-        if (i%2==1) F[i]+=i*i;
+    ll t; cin>> t;
+    for (ll i=1; i<=1000; i++){
+        F[i][i]=1;
+        F[i][1]=i;
     }
-    cin>> t;
+    for (ll i=3; i<=1000; i++){
+        for (ll j=2; j<i; j++){
+            F[i][j]=(F[i-1][j]+F[i-1][j-1])%MOD;
+        }
+    }
     while (t--){
-        cin>> n;
-        cout<< F[n]<< "\n";
+        cin>> n>> k;
+        cout<< F[n][k]<< "\n";
     }
     return 0;
 }
