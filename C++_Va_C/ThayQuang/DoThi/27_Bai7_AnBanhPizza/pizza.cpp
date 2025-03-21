7b25d9c483c3e8285290e6df91d41bb3bf3e129f
chitamha <hachitam.me@gmail.com>
2025-03-21 22:42:26 +0700

    21_03_2025

--- C++_Va_C/ThayQuang/DoThi/27_Bai7_AnBanhPizza/pizza.cpp
+++ C++_Va_C/ThayQuang/DoThi/27_Bai7_AnBanhPizza/pizza.cpp
@@ -3,20 +3,16 @@
 #define se second
 using namespace std;
 
-int n, m, q, hmany, Price[100005], visited[100005];
-double Bill[100005];
+int n, m, q, cnt, Price[100005], visited[100005];
 vector<int> adj[100005];
-vector<pair<int, int>> Ate;
-set<int> se;
+map<int, int> mp;
 
-void DFS(int u, int which, int siz){
-    if (siz!=0) Bill[u]=1.0*Price[which]/siz;
-    //if (siz!=0) cout<< u<< " ";
-    hmany++;
-    visited[u]=1;
+void DFS(int u, int which){
+    cnt++;
+    visited[u]=which;
     for (int v:adj[u]){
         if (!visited[v])
-            DFS(v, which, siz);
+            DFS(v, which);
     }
 }
 
@@ -27,22 +23,19 @@ int main(){
     cin.tie(nullptr); cout.tie(nullptr);
     cin>> n>> m>> q;
     for (int i=1; i<=m; i++) cin>> Price[i];
+    map<int, int> tmp;
     for (int i=1; i<=q; i++){
         int typ, x, y;
         cin>> typ>> x>> y;
-        if (typ==1) se.insert(y), Ate.push_back({x, y});
+        if (typ==1) tmp[y]=x;
         else adj[x].push_back(y), adj[y].push_back(x); {}
     }
-    for (pair<int, int> p:Ate){
-        hmany=0;
-        memset(visited, 0, sizeof visited);
-        DFS(p.fi, p.se, 0);
-        memset(visited, 0, sizeof visited);
-        //cout<< p.se<< ": ";
-        DFS(p.fi, p.se, hmany);
-        //cout<< "\n";
+    for (auto p:tmp){
+        cnt=0;
+        DFS(p.se, p.fi);
+        if (mp.count(p.fi)==0) mp[p.fi]=cnt;
     }
-    cout<< se.size()<< "\n";
-    for (int i=1; i<=n; i++) cout<< fixed<< setprecision(8)<< Bill[i]<< "\n";
+    cout<< mp.size()<< "\n";
+    for (int i=1; i<=n; i++) cout<< fixed<< setprecision(8)<< 1.0*Price[visited[i]]/mp[visited[i]]<< "\n";
     return 0;
 }
