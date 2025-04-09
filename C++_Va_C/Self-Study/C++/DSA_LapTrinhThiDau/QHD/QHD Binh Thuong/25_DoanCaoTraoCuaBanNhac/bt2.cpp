349ab662c5803929cb4c45f909b3b4a0103b9360
chitamha <hachitam.me@gmail.com>
2025-04-09 17:22:17 +0700

    09_04_2025

--- C++_Va_C/Self-Study/C++/DSA_LapTrinhThiDau/QHD/QHD Binh Thuong/25_DoanCaoTraoCuaBanNhac/bt2.cpp
+++ C++_Va_C/Self-Study/C++/DSA_LapTrinhThiDau/QHD/QHD Binh Thuong/25_DoanCaoTraoCuaBanNhac/bt2.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 int n, A[maxn];
 int F[maxn][maxn];
-//Gọi F[i][j] là đoạn cao trào dài nhất của bản nhạc xét từ 1... i và i+5... j
+//Gọi F[i][j]+1 là đoạn cao trào dài nhất của bản nhạc xét từ 1... i và i+5... j
 //Với tư tưởng của bài “xâu con chung dài nhất” ta có thể giải quyết bài toán trên!
 //Kết quả là Max(F[i][j])
 
@@ -27,17 +27,15 @@ int main(){
     cin.tie(0); cout.tie(0);
     cin>> n;
     for (int i=1; i<=n; i++) cin>> A[i];
-    for (int i=1; i<=n; i++) F[1][i]=1;
-    //Bài toán con nhỏ nhất: Độ dài đoạn cao trào là 1
-    int res=1;
+    int res=0;
     //2 vòng lặp for này đảm bảo đoạn cao trào dài nhất xuất hiện ít nhất 2 lần
-    for (int i=1; i<=n-5; i++){
+    for (int i=2; i<=n-5; i++){
         for (int j=i+5; j<=n; j++){
             if (A[i]-A[i-1]==A[j]-A[j-1]){
                 F[i][j]=F[i-1][j-1]+1;
-                res=max(res, min(F[i][j], j-i));
+                res=max(res, min(F[i][j]+1, j-i));
                 //Các đoạn không được chồng lên nhau nên j-i
-            } else F[i][j]=1;
+            }
         }
     }
     //Đoạn cao trào phải có từ 5 nốt nhạc trở lên
